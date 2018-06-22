0255e7964c506de64a3fec9d9bd4379514093edf
ashishrastogise <ashish.rastogi.se@gmail.com>
2018-06-22 22:24:45 +0530

    Updated code to make to executable from provided tomcat.

--- src/main/java/com/dev/MainApp.java
+++ src/main/java/com/dev/MainApp.java
@@ -2,10 +2,18 @@ package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApp {
-   public static void main(String[] args) {
-      SpringApplication.run(MainApp.class, args);
-   }
+public class MainApp extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApp.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
 }
--- src/main/java/com/dev/util/SMSSenderUtil.java
+++ src/main/java/com/dev/util/SMSSenderUtil.java
@@ -14,9 +14,10 @@ public class SMSSenderUtil {
 	public static String sendMessageToKannel (SMS sms) throws IOException {
 		// HTTP GET request
 		String messageId = sms.getNumber()+"GUI"+getRandomNumberInRange(100000, 1000000);
+		
 		String dlrUrl = "http://52.66.156.150:8080/noesis-smpp/DLRRequestListener?dr=%a&smscid=%i&statuscd=%d&uniqID="+messageId+"&customerref=ashish"+
-				"&recivetime="+System.currentTimeMillis()+"&dlrtype={9}&mobile="+sms.getNumber()+"&submittime="+System.currentTimeMillis()
-				+"&expiry={12}senderid=NOESIS&carrierid=TELCONAME&circleid=DELHI&routeid=DELHI&systemid=%o";
+				"&recivetime="+System.currentTimeMillis()+"&dlrtype=9&mobile="+sms.getNumber()+"&submittime="+System.currentTimeMillis()
+				+"&expiry=12senderid=NOESIS&carrierid=TELCONAME&circleid=DELHI&routeid=DELHI&systemid=%o";
 
 		String url = "http://52.66.156.150:13013/cgi-bin/sendsms?username=test&password=test&smscid=test&from=NOESIS&to="+URLEncoder.encode(String.valueOf(sms.getNumber()),"UTF-8")+
 				"&text="+URLEncoder.encode(sms.getMessage(),"UTF-8")+"&coding=0&dlr-mask=23&dlr-url="+URLEncoder.encode(dlrUrl, "UTF-8");
